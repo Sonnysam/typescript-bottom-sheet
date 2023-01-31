2239e2cd6c7a6fec7d9d5ba24d648a8ec6ef3275
sonnysam <samuelagbenyo067@gmail.com>
2023-01-31 01:10:22 -0800

    bottomsheet added

---
+++ components/BottomComponent.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useRef, useState } from "react";
+import tw from "twrnc";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+
+export default function BottomComponent() {
+  const sheetRef = useRef<BottomSheet>(null);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const snapPoints = ["25%", "50%", "75%"];
+  
+
+  return (
+    <>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        enablePanDownToClose={true}
+        onClose={() => setIsOpen(false)}
+      >
+        <BottomSheetView children>
+          <Text>BottomSheetView</Text>
+        </BottomSheetView>
+      </BottomSheet>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
--- components/Maps.tsx
+++ components/Maps.tsx
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import tw from "twrnc"
+
 
 export default function Maps() {
   return (
